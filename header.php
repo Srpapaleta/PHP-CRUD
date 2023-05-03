7cf8daee4e7fd6471c94e55ae8c0410532aa8207
Javier Carbonell <95599570+Srpapaleta@users.noreply.github.com>
2023-05-02 22:00:44 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--- BOOTSTRAP 4 --->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!--- font awesome 5--->
+    <link href="/your_path_to_version_6_compiled_files/css/fontawesome.css" rel="stylesheet"> 
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+    <a href="index.php" class="navbar-brand">PHP CRUD</a>
+    </div>
+</nav>
